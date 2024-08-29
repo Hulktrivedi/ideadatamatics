9d015bb8f6bb12f49cff41c8121945863afad82b
root <root@MrTrivedi>
2024-08-29 15:41:01 +0530

    bugfix0.4

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-'use client'
 /* eslint-disable react/no-unescaped-entities */
+'use client'
+
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
